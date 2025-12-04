7f904640ff266cfdc3d3a4b68277eb6a3a6a6706
elisemariehals <elisemariehals@gmail.com>
2025-12-04 17:08:55 +0100

    Noen endringer i main.c, og prj.conf, prøvde å få LED på thingy til å blinke men det skjedde ikke.

--- node_A/src/main.c
+++ node_A/src/main.c
@@ -1,6 +1,32 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 alias is not defined"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+    int ret;
+
+    ret = gpio_is_ready_dt(&led);
+    if (!ret) {
         return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(500);
+    }
 }
+
