31fb687916f48a387dde32e0440fdca78a79dd87
elisemariehals <elisemariehals@gmail.com>
2025-12-06 17:30:18 +0100

    Oppdaterer node A ...

--- node_A/src/main.c
+++ node_A/src/main.c
@@ -1,31 +1,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-#define LED0_NODE DT_ALIAS(led0)
-
-#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#error "Unsupported board: led0 alias is not defined"
-#endif
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
 void main(void)
 {
-    int ret;
-
-    if (!gpio_is_ready_dt(&led)) {
-        printk("GPIO not ready\n");
-        return;
-    }
-
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        printk("Failed to configure LED\n");
-        return;
-    }
+    printk("Program started!\n");
 
     while (1) {
-        printk("Hello from Thingy!\n");
-        k_msleep(1000);   // vent 1 sekund
+        printk("Still running...\n");
+        k_msleep(1000);
     }
 }
+
