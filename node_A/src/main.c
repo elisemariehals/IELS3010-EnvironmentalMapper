848bae60f1d094339645618313f7084b4ee21b38
snipa <anna.klauseie@gmail.com>
2025-12-06 16:39:21 +0100

    led

--- node_A/src/main.c
+++ node_A/src/main.c
@@ -2,10 +2,10 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(status_led0)
 
 #if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#error "Unsupported board: led0 alias is not defined"
+#error "Unsupported board: status_led0 alias is not defined"
 #endif
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -14,13 +14,14 @@ int main(void)
 {
     int ret;
 
-    ret = gpio_is_ready_dt(&led);
-    if (!ret) {
+    if (!gpio_is_ready_dt(&led)) {
+        printk("GPIO not ready\n");
         return 0;
     }
 
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
+        printk("Failed to configure LED\n");
         return 0;
     }
 
@@ -29,4 +30,3 @@ int main(void)
         k_msleep(500);
     }
 }
-
